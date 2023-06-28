db4baa1375eab433d1d57054aaed3115fd5aa3d4
Kirill Snopko <kirillsnopko@gmail.com>
2023-06-28 09:56:31 +0300

    updated

--- LogicLayer/Dto/gradeLevel/GradeLevelDto.cs
+++ LogicLayer/Dto/gradeLevel/GradeLevelDto.cs
@@ -8,6 +8,6 @@ namespace LogicLayer.Dto.gradeLevel
         public int SubjectId { get; set; }
         public int Level { get; set; }
         public string Description { get; set; }
-        public List<Topic> Topics { get; set; }
+        public int Count { get; set; }
     }
 }
