9e796bdcc67d5ae665559d1e2e6559224fd943d4
Kirill Snopko <kirillsnopko@gmail.com>
2023-06-28 09:55:42 +0300

    add topic service

---
+++ LogicLayer/Mapper/TopicMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DataAccessLayer.Entity;
+using LogicLayer.Dto.topic;
+
+
+namespace LogicLayer.Mapper
+{
+    public class TopicMapper:Profile
+    {
+        public TopicMapper()
+        {
+            CreateMap<TopicCreateDto, Topic>();
+            CreateMap<Topic, TopicDto>();
+        }
+    }
+}
---
+++ LogicLayer/Service/TopicService.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DataAccessLayer.Entity;
+using DataAccessLayer.Repository;
+using LogicLayer.Dto.topic;
+using LogicLayer.Service.Base;
+
+
+namespace LogicLayer.Service
+{
+    public class TopicService : ServiceMain<Topic, TopicDto, TopicCreateDto>
+    {
+        public TopicService(RepositoryBase<Topic> topicRepository, IMapper mapper) : base(topicRepository, mapper) { }
+    }
+}
